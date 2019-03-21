f966d8dd10b086a9480d9517db3653d3af4fbd45
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-03-21 01:51:34 -0700

    committed

--- vars/PropertyReader.groovy
+++ vars/PropertyReader.groovy
@@ -1,7 +1,20 @@
 class PropertyReader {
 
    String filePath
-   PropertyReader(String filePath) {
+   PropertyReader()
+   {
+   
+   }
+   public PropertyReader(String filePath) {
         this.filePath = filePath
-	    }
+	  returnData(filePath)
+    }
+	String returnData(Path)
+	{
+	InputStream input = null;
+	input = new FileInputStream(Path);
+	return prop	
+	prop.load(input);
+	}
+    
 }
