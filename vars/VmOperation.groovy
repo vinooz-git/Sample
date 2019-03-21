2793f2ade89b040fdd87a88d96230a9a42b8d949
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-03-21 05:37:15 -0700

    file updated

--- vars/VmOperation.groovy
+++ vars/VmOperation.groovy
@@ -1,7 +1,7 @@
 def call() {
 def prop = new PropertyReader();
 def propert = prop.returnData("${WORKSPACE}/pipeline.properties")
-if(propert.VmPowerOn == Yes)
+if(propert.VmPowerOn == 'Yes')
 {
 vSphere buildStep: [$class: 'PowerOn', timeoutInSeconds: 180, vm: 'pa-tst3-w10'], serverName: 'Neptune'
 }
