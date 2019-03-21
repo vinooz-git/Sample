bfc6d703f10d098f9d38a7842a5fef37a68230f8
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-03-21 06:08:44 -0700

    issue fixed

--- vars/VmOperation.groovy
+++ vars/VmOperation.groovy
@@ -1,22 +1,23 @@
+@NonCPS
 def call() {
 def vmname = null;
 def prop = new PropertyReader();
 def propert = prop.returnData("${WORKSPACE}/pipeline.properties")
 if(propert.VmPowerOn == 'Yes')
 {
-vmname = propert.VmName
-vSphere buildStep: [$class: 'PowerOn', timeoutInSeconds: 300, vm: vmname], serverName: 'Neptune'
+	vmname = propert.VmName
+	vSphere buildStep: [$class: 'PowerOn', timeoutInSeconds: 300, vm: vmname], serverName: 'Neptune'
 }
 
 if(propert.VmPowerOff == 'Yes')
 {
-vmname = propert.VmName
-vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: vmname], serverName: 'Neptune'
+	vmname = propert.VmName
+	vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: vmname], serverName: 'Neptune'
 }
 
 if(propert.VmRevert == 'Yes')
 {
-vmname = propert.VmName
+	vmname = propert.VmName
 }
 
 }
