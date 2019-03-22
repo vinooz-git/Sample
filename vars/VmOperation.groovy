58cf4d45ec5a2cd329ba58c8103e6c17ea3ab4e2
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-03-21 23:09:54 -0700

    step updated

--- vars/VmOperation.groovy
+++ vars/VmOperation.groovy
@@ -1,21 +1,23 @@
-def call() {
+def call() 
+{
 def prop = new PropertyReader();
 def propert = prop.returnData("${WORKSPACE}/pipeline.properties")
-if(propert.VmPowerOn == 'Yes')
-{
-	def vmname = propert.VmName
-	vSphere buildStep: [$class: 'PowerOn', timeoutInSeconds: 300, vm: vmname], serverName: 'Neptune'
-}
+stage
+  {
+	if(propert.VmPowerOn == 'Yes')
+	{
+		def vmname = propert.VmName
+		vSphere buildStep: [$class: 'PowerOn', timeoutInSeconds: 300, vm: vmname], serverName: 'Neptune'
+	}
+	if(propert.VmPowerOff == 'Yes')
+	{
+		def vmname1 = propert.VmName
+		vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: vmname1], serverName: 'Neptune'
+	}
 
-if(propert.VmPowerOff == 'Yes')
-{
-	def vmname1 = propert.VmName
-	vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: vmname1], serverName: 'Neptune'
-}
-
-if(propert.VmRevert == 'Yes')
-{
+	if(propert.VmRevert == 'Yes')
+	{
 	vmname = propert.VmName
-}
-
+	}
+  }
 }
