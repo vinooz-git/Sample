b9e5a1286f24e18eca48b14aa03d65bea773c075
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-03-22 01:38:18 -0700

    File Changed

--- vars/VmOperation.groovy
+++ vars/VmOperation.groovy
@@ -1,22 +1,29 @@
 import com.cloudbees.groovy.cps.NonCPS
 def call() 
 {
- def VmName = 'pa-tst3-w10'
-def propert = 'Yes'
-if(propert == 'Yes')
+def prop = new PropertyReader();
+def propert = prop.returnData("${WORKSPACE}/pipeline.properties")
+def VmName = 'pa-tst3-w10'
+if(propert.VmPowerOff == 'Yes')
 	{
-	VmSwitchOn(VmName)
+	VmSwitchOff(VmName)
 	}
 }	
 @NonCPS
 def VmSwitchOn(vmname1)
 {
-	try
-	{
-		vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: vmname1], serverName: 'Neptune'
-	}
-	catch(Exception e)
-	{
+	vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: vmname1], serverName: 'Neptune'
 	
-	}
+}
+@NonCPS
+def VmSwitchOff(vmname1)
+{
+	vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: vmname1], serverName: 'Neptune'
+	
+}
+@NonCPS
+def VmRevert(vmname1)
+{
+	vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: vmname1], serverName: 'Neptune'
+	
 }
