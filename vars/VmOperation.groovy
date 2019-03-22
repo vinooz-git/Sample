f37ead0dff89e05918d3f3ec968f8eccab0b5dd0
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-03-22 00:20:46 -0700

    no changes

--- vars/VmOperation.groovy
+++ vars/VmOperation.groovy
@@ -7,11 +7,10 @@ if(propert.VmPowerOff == 'Yes')
 	{
 	VmSwitchOn()
 	}
-		
+}	
 @NonCPS
 def VmSwitchOn()
 {
 def vmname1 = propert.VmName
 		vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: vmname1], serverName: 'Neptune'
-}
 }
