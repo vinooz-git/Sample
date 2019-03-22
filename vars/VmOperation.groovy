e0417dc544b8e61aa14171118ca7d13e8a382fa6
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-03-22 03:20:45 -0700

    commited

--- vars/VmOperation.groovy
+++ vars/VmOperation.groovy
@@ -3,23 +3,28 @@ def call()
 {
 def propert = new PropertyReader()
 def VmName = 'pa-tst3-w10'
-if(propert.VmPowerOn == 'Yes')
-{
-	VmSwitchOn(VmName)
-}
+if(propert.VmPowerOff == 'Yes')
+	{
+	VmSwitchOff(VmName)
+	}
+	
 }	
+
 @NonCPS
 def VmSwitchOn(vmname1)
 {
-	vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: VmName], serverName: 'Neptune'
+	vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: vmname1], serverName: 'Neptune'
+	
 }
 @NonCPS
 def VmSwitchOff(vmname1)
 {
 	vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: vmname1], serverName: 'Neptune'
+	
 }
 @NonCPS
 def VmRevert(vmname1)
 {
 	vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: vmname1], serverName: 'Neptune'
+	
 }
