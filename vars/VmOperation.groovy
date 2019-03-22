ada35732c88c1403d588e4bf41698fabef622819
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-03-21 23:33:59 -0700

    code changed

--- vars/VmOperation.groovy
+++ vars/VmOperation.groovy
@@ -8,12 +8,7 @@ def propert = prop.returnData("${WORKSPACE}/pipeline.properties")
 		def vmname = propert.VmName
 		vSphere buildStep: [$class: 'PowerOn', timeoutInSeconds: 300, vm: vmname], serverName: 'Neptune'
 	}
-	def vmname = null
-	if(propert.VmPowerOff == 'Yes')
-	{
-		def vmname1 = propert.VmName
-		vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: vmname1], serverName: 'Neptune'
-	}
-	def vmname1 = null
-  
+	vmname = null  
+	propert = null
+	prop = null
 }
