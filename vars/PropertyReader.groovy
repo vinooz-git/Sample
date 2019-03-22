0a932bfc887e12a258a3e71bf4a279e304b6a7ee
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-03-22 02:48:07 -0700

    File changed

--- vars/PropertyReader.groovy
+++ vars/PropertyReader.groovy
@@ -9,7 +9,7 @@ def returnDatas()
 {
 InputStream input = null;
     Properties prop = new Properties();
-	input = new FileInputStream("${WORKSPACE}/pipeline.properties");
+	input = new FileInputStream("C:\Program Files (x86)\Jenkins\workspace\AdminPipeLine\pipeline.properties");
 	prop.load(input);
 	return prop	 
 }
