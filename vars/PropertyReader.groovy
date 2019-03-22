57bb8aa93eb8ac2b27a0a1335ccbf14956101756
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-03-22 01:50:49 -0700

    New changes commmited

--- vars/PropertyReader.groovy
+++ vars/PropertyReader.groovy
@@ -2,9 +2,14 @@ class PropertyReader
 {
 	InputStream input = null;
     String filePath
-	
-
  String returnData(filePath)
+	{
+	def data = readData(filePath)
+	return data
+	}
+}
+@NonCPS
+ String readData(filePath)
 {
 	Properties prop = new Properties();
 	input = new FileInputStream(filePath);
