6b128483fa8dd6174f3585339c60e021bc7091e8
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-03-25 08:29:38 -0500

    New machine configured

--- vars/PropertyReader.groovy
+++ vars/PropertyReader.groovy
@@ -6,7 +6,7 @@ class ProertyReader
 	{
 		InputStream input = null;
 		Properties prop = new Properties();
-		input = new FileInputStream("C:\\Program Files (x86)\\Jenkins\\workspace\\AdminPipeLine\\pipeline.properties");
+		input = new FileInputStream("D:\\Jenkins\\pipeline.properties");
 		prop.load(input);
 		return prop	 
 	}
