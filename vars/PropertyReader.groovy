d7ea759140b9759a8caa0b25fa8c4afd9959f1a2
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-04-01 08:55:58 -0500

    old changes

--- vars/PropertyReader.groovy
+++ vars/PropertyReader.groovy
@@ -1,8 +1,14 @@
 class ProertyReader
 {
-def path
-	def returnDatas(String path)
-	{	println "Path Value is"+path
+	public ProertyReader()
+	{returnDatas()}
+	def returnDatas()
+	{
+		node 
+		{
+			def workspace = env.WORKSPACE
+			println"workspace path is "+workspace
+		}
 		InputStream input = null;
 		Properties prop = new Properties();
 		input = new FileInputStream("D:\\Jenkins\\pipeline.properties");
