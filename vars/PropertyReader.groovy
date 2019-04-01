f4f77127995bee842f4a8a39cead514427fb15f5
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-04-01 05:34:36 -0500

    Changes commited

--- vars/PropertyReader.groovy
+++ vars/PropertyReader.groovy
@@ -6,7 +6,7 @@ class ProertyReader
 	{
 		InputStream input = null;
 		Properties prop = new Properties();
-		input = new FileInputStream(""${env.WORKSPACE}"+"\\pipeline.properties"");
+		input = new FileInputStream("${env.WORKSPACE}"+"\\pipeline.properties");
 		
 		prop.load(input);
 		return prop	 
