db09fb2e1abe54dd08dedbe12e4d0215cafae082
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-04-02 02:01:37 -0500

    new changes

--- vars/JenkinsPipeline.groovy
+++ vars/JenkinsPipeline.groovy
@@ -10,7 +10,7 @@ def call()
 	}
     stage('BuildDownload')
 	{
-		node('pa-tst4-w7') 
+		node
 		{
 			checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: '3c8a141a-24e6-4010-a36b-8ab9209b42d8', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: 'http://brdc-svn.network.internal/svn/AutomationFrameworks/branches/7.1_Universal%20Viewer/Functional_Automation/Scripts/Selenium/Scripts']], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
 		}
