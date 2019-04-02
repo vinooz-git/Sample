bac0a02b0bfec6209a1df438a29036442592a6e4
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-04-02 00:54:48 -0500

    worst try

--- vars/PropertyReader.groovy
+++ vars/PropertyReader.groovy
@@ -4,12 +4,11 @@ def call()
 		{ // To get environmental value
 			def workspace = env.WORKSPACE
 			println"workspace path is "+workspace
-		}
 		InputStream input = null;
 		Properties prop = new Properties();
 		println"workspace path is "+workspace
 		input = new FileInputStream(workspace.toString()+"\\pipelineJob.properties");
 		prop.load(input);
 		return prop	 
-
+        }
 }
