db34642f1dd04fd245ab60c79191f2b2adf15b9a
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-04-02 00:53:40 -0500

    try

--- vars/PropertyReader.groovy
+++ vars/PropertyReader.groovy
@@ -7,6 +7,7 @@ def call()
 		}
 		InputStream input = null;
 		Properties prop = new Properties();
+		println"workspace path is "+workspace
 		input = new FileInputStream(workspace.toString()+"\\pipelineJob.properties");
 		prop.load(input);
 		return prop	 
