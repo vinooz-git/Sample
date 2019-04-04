937b2dd0126ca966b0e7f687d2c228c07c9217bc
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-04-04 04:16:12 -0500

    Xml File read Test

--- vars/VmOperation.groovy
+++ vars/VmOperation.groovy
@@ -1,24 +1,28 @@
 import com.cloudbees.groovy.cps.NonCPS
+import groovy.util.XmlSlurper
 def call() 
 {
-def propert = PropertyReader()
+def file = new File("D:\\Jenkins\\PipelineProperties.xml")
+def config = new XmlSlurper().parse(file)
+String VMList
+VMList = config.VmOperation.RevertMachine.NEPTUNE;
+println"Vm list is "+VMList
 def VmName = propert.VmNames
-println "vm name is"+VmName
 def snapshot = propert.snapshotName
 def Network = propert.Network
-	if(propert.VmPowerOn == 'Yes')
-		{
-		vSphere buildStep: [$class: 'PowerOn', timeoutInSeconds: 260, vm: VmName], serverName: Network
-		echo "${VmName} is Switched ON"
-		}
-	if(propert.VmPowerOff == 'Yes')
-		{
-		vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: VmName], serverName: Network
-		echo "${VmName} is Switched Off"
-		}
-	if(propert.VmRevert == 'Yes')
-		{
-		vSphere buildStep: [$class: 'RevertToSnapshot', snapshotName: snapshot, vm: VmName], serverName: Network
-		echo "${VmName} is Reverted to ${snapshot} - Snapshot"
-		}
+if(propert.VmPowerOn == 'Yes')
+	{
+	vSphere buildStep: [$class: 'PowerOn', timeoutInSeconds: 260, vm: VmName], serverName: Network
+	echo "${VmName} is Switched ON"
+	}
+if(propert.VmPowerOff == 'Yes')
+	{
+	vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: VmName], serverName: Network
+	echo "${VmName} is Switched Off"
+	}
+if(propert.VmRevert == 'Yes')
+	{
+	vSphere buildStep: [$class: 'RevertToSnapshot', snapshotName: snapshot, vm: VmName], serverName: Network
+	echo "${VmName} is Reverted to ${snapshot} - Snapshot"
+	}
 }	
