26f0f75d0561d5b05fad00a9148c44a50b301c36
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-04-04 05:33:29 -0500

    Csv reader concept implemented

--- vars/VmOperation.groovy
+++ vars/VmOperation.groovy
@@ -2,25 +2,41 @@ import com.cloudbees.groovy.cps.NonCPS
 import groovy.util.XmlSlurper
 def call() 
 {
-def file = new File("D:\\Jenkins\\PipelineProperties.xml")
-def config = new XmlSlurper().parse(file)
+def file = new File("D:\\Jenkins\\VmOperationDetails.csv")
+if (file.exists() && file.isFile()) {
+    String[] lines = file.text.split('\n')
+    List<String[]> rows = lines.collect {
+        it.split(',')
+    }
+	println"the Line is "+rows.toString()
+} else
+ {
+ echo "Exception Occurs, The Csv File not in Valid format  "
+}
 String VMList
-VMList = config.VmOperation.RevertMachine.NEPTUNE;
 println"Vm list is "+VMList
-def VmName = propert.VmNames
-def snapshot = propert.snapshotName
-def Network = propert.Network
-if(propert.VmPowerOn == 'Yes')
+
+@NonCPS
+def VmPowerOn()
 	{
+	VMList = config.VmOperation.PowerOnMachine.VmName;
+	if(VMList!="")
+      {
+            def PowerOn_Machines = [:]
+            String[] addittionalServers = addittionalServersList.split(',');
+			
 	vSphere buildStep: [$class: 'PowerOn', timeoutInSeconds: 260, vm: VmName], serverName: Network
 	echo "${VmName} is Switched ON"
+	  }
 	}
-if(propert.VmPowerOff == 'Yes')
+@NonCPS
+def VmPowerOff()
 	{
 	vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: VmName], serverName: Network
 	echo "${VmName} is Switched Off"
 	}
-if(propert.VmRevert == 'Yes')
+@NonCPS
+def VmRevert()
 	{
 	vSphere buildStep: [$class: 'RevertToSnapshot', snapshotName: snapshot, vm: VmName], serverName: Network
 	echo "${VmName} is Reverted to ${snapshot} - Snapshot"
