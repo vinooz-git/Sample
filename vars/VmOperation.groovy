ff2e9dd0e80919cd59914ffac593ba829161b97b
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-04-04 07:00:26 -0500

    CSV FILE TRY 3

--- vars/VmOperation.groovy
+++ vars/VmOperation.groovy
@@ -3,41 +3,48 @@ import groovy.util.XmlSlurper
 def call() 
 {
 def file = new File("D:\\Jenkins\\VmOperationDetails.csv")
-if (file.exists() && file.isFile()) {
+if (file.exists() && file.isFile()) 
+{
     String[] lines = file.text.split('\n')
-	println"New Line is "+ lines[0]
-    List<String[]> rows = lines.collect {
-        it.split(',')
-    }
-	println"the Line is "+rows.toString()
-} else
- {
- echo "Exception Occurs, The Csv File not in Valid format  "
+	for(int i =1; i<=lines.size(); i++)
+	{
+		def row = lines[i];
+		String[] rowvalues = row.split(',');
+		println"Current row values :"+row
+		if(rowvalues[0].equalsIgnoreCase("VmRevert"))
+			{
+				VmRevert(rowvalues[1],rowvalues[2],rowvalues[3])
+			}
+		if(rowvalues[0].equalsIgnoreCase("VmPowerOn"))
+			{
+				VmPowerOn(rowvalues[1],rowvalues[2])
+			}
+		if(rowvalues[0].equalsIgnoreCase("VmPowerOff"))
+			{
+				VmPowerOff(rowvalues[1],rowvalues[2])
+			}
+	}
+} 
+else
+{
+ echo "Exception Occurs, The Csv File not in Valid format"
 }
-String VMList
-println"Vm list is "+VMList
+
 }
 @NonCPS
-def VmPowerOn()
+def VmPowerOn(VmName,Network)
 	{
-	VMList = config.VmOperation.PowerOnMachine.VmName;
-	if(VMList!="")
-      {
-            def PowerOn_Machines = [:]
-            String[] addittionalServers = addittionalServersList.split(',');
-			
 	vSphere buildStep: [$class: 'PowerOn', timeoutInSeconds: 260, vm: VmName], serverName: Network
 	echo "${VmName} is Switched ON"
-	  }
 	}
 @NonCPS
-def VmPowerOff()
+def VmPowerOff(VmName,Network)
 	{
 	vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: VmName], serverName: Network
 	echo "${VmName} is Switched Off"
 	}
 @NonCPS
-def VmRevert()
+def VmRevert(VmName,Network,Snapshot)
 	{
 	vSphere buildStep: [$class: 'RevertToSnapshot', snapshotName: snapshot, vm: VmName], serverName: Network
 	echo "${VmName} is Reverted to ${snapshot} - Snapshot"
