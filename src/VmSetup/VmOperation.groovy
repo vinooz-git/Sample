75cceb3d07ba68e4025b9f992719abab9b5cac7d
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-04-05 06:34:06 -0500

    Vm Operation Try 1

--- src/VmSetup/VmOperation.groovy
+++ src/VmSetup/VmOperation.groovy
@@ -1,9 +1,38 @@
 package VmSetup
-def VmOperationCall()
-{
-println"Vm operation Method working fine";
-}
+
 def file = new File("D:\\Jenkins\\VmOperationDetails.csv");
+  if (file.exists() && file.isFile()) 
+  {
+    String[] lines = file.text.split('\n')
+	for(int i =1; i<lines.size(); i++)
+	{
+		def row = lines[i];
+		String[] rowvalues = row.split(',');
+		//println"Current row values :"+row
+		def Action = rowvalues[0].trim(); def VmName = rowvalues[1].trim();
+		def Network = rowvalues[2].trim(); def Snapshot = rowvalues[3].trim();
+		if(Action.equalsIgnoreCase("VmRevert"))
+			{
+				VmRevert(VmName,Network,Snapshot)
+				sleep 10;
+			}
+		if(Action.equalsIgnoreCase("VmPowerOn"))
+			{
+				VmPowerOn(VmName,Network)
+				sleep 10;
+			}
+		if(Action.equalsIgnoreCase("VmPowerOff"))
+			{
+				VmPowerOff(VmName,Network)
+				sleep 10;
+			}
+	}
+  } 
+ else
+ {
+ echo "Exception Occurs, The Csv File not in Valid format"
+ }
+
 
 def VmRevert(VmName,Network,Snapshot)
 	{
