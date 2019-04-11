374c0cc675ea7e50f9a4a8002477f1d195fb5092
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-04-11 03:32:25 -0500

    buildoperaion variables changed dynamically

--- src/BuildLibrary/BuildOperation.groovy
+++ src/BuildLibrary/BuildOperation.groovy
@@ -2,15 +2,16 @@ package BuildLibrary
 
 def BuildOperationCall(def propertyFileLoc)
 {
+  def projectname = null;
+  def BuildUrl = null;
+  def BuildCopyLoc = null;
+  def Filename = null;
   def file = new File(propertyFileLoc);
   if (file.exists() && file.isFile()) 
   {
     String[] lines = file.text.split('\n')
 	for(int i =0; i<lines.size(); i++)
 	{
-		def projectname = null;
-		def BuildUrl = null;
-		def BuildCopyLoc = null;
 		def row = lines[i];
 		println"Current Row :"+ row
 		if(row.contains("ProjectName")){String[] rowvalues = row.split('=');projectname =rowvalues[1].trim();println"project name : "+projectname;}		
@@ -18,8 +19,11 @@ def BuildOperationCall(def propertyFileLoc)
 		{
 		String[] rowvalues = row.split('=');
 		BuildUrl = rowvalues[1].trim(); 
+		String[] filenametemp = BuildUrl.split('/');
+		Filename = filenametemp[filenametemp.size()-1]
+		println"File Name :"+Filename
 		println"BuildUrl :"+BuildUrl
-		BuildCopyLoc = "C:\\"+projectname+"_Build";
+		BuildCopyLoc = "C:\\"+projectname+"_Build"+Filename;
 		println"BuildCopyLoc :"+BuildCopyLoc
 		//Download latest build 
 		httpRequest ignoreSslErrors: true, outputFile: BuildCopyLoc, responseHandle: 'NONE', url: BuildUrl
