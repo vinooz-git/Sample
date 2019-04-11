af69b96368b304995ef252e8099f20c52974ec46
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-04-11 06:48:38 -0500

    try5

--- src/BuildLibrary/BuildOperation.groovy
+++ src/BuildLibrary/BuildOperation.groovy
@@ -44,7 +44,7 @@ def getServerList(row)
 	println"Row Value :"+ServerListTemp
 	if(ServerListTemp.contains(","))
 	{
-	serverList = ServerListTemp.split(',');
+	serverList = ServerListTemp.split(',').trim();
 	println"serverList with "+serverList
 	}
 	else{
