ba43ea628e328e58cc6788aabe48ba2ced37c8b4
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-04-15 02:22:13 -0500

    Delete FIles try

--- src/BuildLibrary/BuildOperation.groovy
+++ src/BuildLibrary/BuildOperation.groovy
@@ -30,21 +30,19 @@ def BuildOperationCall(def propertyFileLoc)
 				 //httpRequest ignoreSslErrors: true, outputFile: BuildUrl.get(1), responseHandle: 'NONE', url: BuildUrl.get(0)
 				 
 				 String[] Tempfoldername  = BuildUrl.get(2).split("[.]");
-				 
 				 String FolderName = Tempfoldername[0].replaceAll("%20"," ");
-				 
 				 String CopyFromFolder = BuildOutputLoc +"\\"+FolderName;
 				 println"CopyFromFolder :"+ CopyFromFolder;
 				 def deleteFile = BuildOutputLoc +"\\"+BuildUrl.get(2);
 				 
 				 //Extract the Build
-				 //fileOperations([fileUnZipOperation(filePath: BuildUrl.get(1), targetLocation: BuildOutputLoc)])
+				 fileOperations([fileUnZipOperation(filePath: BuildUrl.get(1), targetLocation: BuildOutputLoc)])
+				 
 				 //Copy File and folder /* This step only for PACS Server Setup*
-				 //bat label: '', script: "((robocopy \"C:\\PACS_build\\8_1_0\\IBM Merge PACS Server Software CD\" ${BuildOutputLoc} /S /MT:100 > C:\\log.txt) ^& IF %ERRORLEVEL% LEQ 4 exit /B 0)"
 				 bat label: '', script: "((robocopy \"${CopyFromFolder}\" ${BuildOutputLoc} /S /MT:100 > C:\\log.txt) ^& IF %ERRORLEVEL% LEQ 4 exit /B 0)"
 				 
 				 //Delete unwanted folders and files
-				 //bat label: '', script: '''DEL /F /Q /A ${deleteFile} RD /S /Q ${CopyFromFolder}'''
+				 bat label: '', script: "(DEL /F /Q /A ${deleteFile} RD /S /Q \"${CopyFromFolder}\")"
 				}
 			  }		
 		   }
