4494398439126d3d580df45aaca2f86496be3d74
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-05-08 09:04:56 -0500

    New Chages

--- src/VmSetup/VmOperation.groovy
+++ src/VmSetup/VmOperation.groovy
@@ -66,8 +66,10 @@ def VmPowerOff(String VmName,String Network)
 	{
 	echo "VmName is ${VmName}"
 	echo "Network is ${Network}"
+	node{
 	//vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: VmName], serverName: Network
 	vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: 'pa-tst4-ws16'], serverName: 'NEPTUNE'
+	}
 	echo "${VmName} is Switched Off"
 	
 	}
