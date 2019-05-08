a38bf339ae4b638a68204c0438c353a5c73764c3
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-05-08 01:26:22 -0500

    New changes

--- src/VmSetup/VmOperation.groovy
+++ src/VmSetup/VmOperation.groovy
@@ -66,7 +66,7 @@ def VmPowerOn(VmName,Network)
 
 def VmPowerOff(VmName,Network)
 	{
-	echo "VmName is ${VmName}"
+	//echo "VmName is ${VmName}"
 	echo "Network is ${Network}"
 	vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: VmName], serverName: Network
 	echo "${VmName} is Switched Off"
