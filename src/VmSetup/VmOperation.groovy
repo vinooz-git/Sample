67e0535513e233cfae8f4a5295c8198cf3e08352
vinooz-git <vinothkumar.palanisamy@ravsoftsolutions.com>
2019-05-10 01:21:48 -0500

    new Changes

--- src/VmSetup/VmOperation.groovy
+++ src/VmSetup/VmOperation.groovy
@@ -4,8 +4,7 @@ Parameters :
   propertyFileLoc - Project pipeline Property file 
 */
 package VmSetup
-def call()
-{
+
 /*
 //PropertyFile Keywords and given variables should be matched
 def poweroff = "VmPowerOff"
@@ -50,7 +49,7 @@ println "File Content:"+fileContent.get(0);
 	}
 	parallel tasks;
   */
-}
+
 @NonCPS
 def VmRevert(VmName,Network,Snapshot)
 	{
@@ -72,15 +71,14 @@ def VmPowerOff(String VmName,String Network)
 		{
 		echo "VmName is ${VmName}"
 		echo "Network is ${Network}"
-	 
-		vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: VmName], serverName: Network
+	    //vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: VmName], serverName: Network
+		httpRequest ignoreSslErrors: true, outputFile: 'C:\PACS_build\8_1_0', responseHandle: 'NONE', url: 'http://10.4.16.25:8080/job/CURRENT_FULL/lastSuccessfulBuild/artifact/server/cds/IBM%20Merge%20PACS%20Server%20Software%20CD/*zip*/IBM%20Merge%20PACS%20Server%20Software%20CD.zip,pa-tst4-ws16)'
 		//vSphere buildStep: [$class: 'PowerOff', evenIfSuspended: false, ignoreIfNotExists: false, shutdownGracefully: false, vm: 'pa-tst4-ws16'], serverName: 'NEPTUNE'
-	 
 		echo "${VmName} is Switched Off"
 		}
 		catch(Exception e)
 		{
-			echo "Exception Occured ${e}"
+			echo "Exception Occurred ${e}"
 		}
 	
 	}
